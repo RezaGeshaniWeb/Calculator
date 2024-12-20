0ba0c28af44d250b6c9328d4fd5c9e9a467032f0
Reza Geshani <rezageshaniweb@gmail.com>
2024-12-21 01:41:49 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let result = document.getElementById('result')
+
+function getResult(value) {
+    if (value === '=') result.value = eval(result.value)
+    else result.value += value
+}
+
+function clearResult() {
+    result.value = ''
+}
